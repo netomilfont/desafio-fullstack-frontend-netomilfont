75ba765636f38d5fb500936a9b1671c00ca4ab1c
netomilfont <netomilfont@hotmail.com>
2023-03-29 18:44:43 -0300

    feat: add page login

---
+++ user-contact/src/pages/Login/index.ts
@@ -0,0 +1,9 @@
+import { useNavigate } from "react-router-dom";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import YupPassword from "yup-password";
+import { useContext, useState } from "react";
+
+const Login = () => {};
+
+export default Login;
